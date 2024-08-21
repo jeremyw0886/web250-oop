1b9ff6892fd564ba1602cae727c7ca69b22c2605
jeremyw0886 <jeremyw0815@icloud.com>
2024-08-21 15:21:13 -0400

    Finished the Bike and Bird Classes.

--- asgn01/bird-challenge/index.php
+++ asgn01/bird-challenge/index.php
@@ -5,19 +5,46 @@ class bird {
   var $food = 'bugs';
   var $nest_placement = 'trees';
   var $conservation_level;
+  var $song;
 
-  function songs($args) {
-
+  function songs() {
+    return $this->song;
   }
 
-  function canFly($args) {
-
+  function canFly() {
+    return 'This bird can fly';
   }
 }
 
 $bird1 = new bird;
+$bird1->common_name = 'Eastern Towhee';
+$bird1->food = 'seeds, fruits, insects, and spiders.';
+$bird1->nest_placement = 'Ground';
+$bird1->conservation_level = 'Low';
+$bird1->song = 'Drink-your-tea';
 
 $bird2 = new bird;
+$bird2->common_name = 'Indigo Bunting';
+$bird2->food = 'small seeds, berries, buds, and insects';
+$bird2->nest_placement = 'roadsides, railroad rights-of-way, and on the edges of cultivated fields';
+$bird2->conservation_level = 'Low';
+$bird2->song = 'whatwhat!!';
+
+echo $bird1->common_name . "<br>";
+echo "Food: " . $bird1->food . "<br>";
+echo "Nest Placement: " . $bird1->nest_placement . "<br>";
+echo "Conservation Level: " . $bird1->conservation_level . "<br>";
+echo "Song: " . $bird1->songs() . "<br>";
+echo $bird1->canFly() . "<br>";
+
+echo "<br>";
+
+echo $bird2->common_name . "<br>";
+echo "Food: " . $bird2->food . "<br>";
+echo "Nest Placement: " . $bird2->nest_placement . "<br>";
+echo "Conservation Level: " . $bird2->conservation_level . "<br>";
+echo "Song: " . $bird2->songs() . "<br>";
+echo $bird2->canFly() . "<br>";
 
 ?>
 
