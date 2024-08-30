b9ca7b592ad31ecbb54cddbd06950f2b9dd80dd7
jeremyw0886 <jeremyw0815@icloud.com>
2024-08-30 12:25:13 -0400

    Add visibility modifiers to the Bicycle class

--- challenge-03-oac.php
+++ challenge-03-oac.php
@@ -2,11 +2,11 @@
 
 class Bicycle {
 
-  var $brand;
-  var $model;
-  var $year;
-  var $description = 'Used bicycle';
-  var $weight_kg = 0.0;
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  public $weight_kg = 0.0;
 
   function name() {
     return $this->brand . " " . $this->model . " (" . $this->year . ")";
