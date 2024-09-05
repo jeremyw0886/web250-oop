34f2309f60c49a76f7f7c8fcafc35b5efc18aad6
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-05 12:00:26 -0400

    Changes canFly() method to a ternary operator.

--- asgn03/Bird.php
+++ asgn03/Bird.php
@@ -1,6 +1,11 @@
 <?php
 
 class Bird {
+
+    public static $instanceCount; // A static property to keep track of the number of instances of the class
+
+    public static $eggNum = 0; // A static property to keep track of the number of eggs laid by all birds
+
     public $habitat;
     public $food;
     public $nesting = "tree";
@@ -8,13 +13,17 @@ class Bird {
     public $song = "chirp";
     public $flying = "yes";
 
+    // public function canFly() {
+    //     if ( $this->flying == "yes" ) {
+    //         $flyingString = "can fly";
+    //     } else {
+    //         $flyingString = "is stuck on the ground";
+    //     }
+    //     return  $flyingString ;
+    // }
+
     public function canFly() {
-        if ( $this->flying == "yes" ) {
-            $flyingString = "can fly";
-        } else {
-            $flyingString = "is stuck on the ground";
-        }
-        return  $flyingString ;
+        return $this->flying == 'yes' ? 'bird can fly' : 'cannot fly and is stuck on the ground'; // ternary operator
     }
 }
 
