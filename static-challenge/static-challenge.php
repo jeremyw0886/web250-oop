eb075d89e1af21185e10a4803a13c108f64a4a0a
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-04 20:12:31 -0400

    Add bicycle class and unicycle subclass from challenge 3 to static-challenge.php file.

---
+++ static-challenge/static-challenge.php
@@ -0,0 +1,42 @@
+<?php
+
+class Bicycle {
+
+  public $brand;
+  public $model;
+  public $year;
+  public $description = 'Used bicycle';
+  private $weightKg = 0.0;
+  protected $wheels = 2;
+
+  public function name() {
+    return $this->brand . " " . $this->model . " (" . $this->year . ")";
+  }
+
+  public function wheelDetails() {
+    $wheelString = $this->wheels == 1 ? "1 wheel" : "{$this->wheels} wheels";
+    return "It has " . $wheelString . ".";
+  }
+
+  public function getWeightKg() {
+    return $this->weightKg . ' kg';
+  }
+
+  public function setWeightKg($value) {
+    $this->weightKg = floatval($value);
+  }
+
+  public function weightLbs() {
+    return floatval($this->weightKg) * 2.2046226218 . ' lbs';
+  }
+
+  public function setWeightLbs($value) {
+    $this->weightKg = floatval($value) / 2.2046226218;
+  }
+
+}
+
+// Create a Unicycle subclass
+class Unicycle extends Bicycle {
+  protected $wheels = 1;
+}
