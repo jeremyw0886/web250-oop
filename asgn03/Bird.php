aec80552f6a0c646b06b87de25c14d2400734cc3
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-05 12:41:59 -0400

    Cleans code, Bird Challenge Complete.

--- asgn03/Bird.php
+++ asgn03/Bird.php
@@ -2,16 +2,16 @@
 
 class Bird {
 
-    public static $instanceCount; // A static property to keep track of the number of instances of the class
+public static $instanceCount; // A static property to keep track of the number of instances of the class
 
-    public static $eggNum = 0; // A static property to keep track of the number of eggs laid by all birds
+public static $eggNum = 0; // A static property to keep track of the number of eggs laid by all birds
 
-    public $habitat;
-    public $food;
-    public $nesting = "tree";
-    public $conservation;
-    public $song = "chirp";
-    public $flying = "yes";
+public $habitat;
+public $food;
+public $nesting = "tree";
+public $conservation;
+public $song = "chirp";
+public $flying = "yes";
 
     // public function canFly() {
     //     if ( $this->flying == "yes" ) {
@@ -22,29 +22,29 @@ class Bird {
     //     return  $flyingString ;
     // }
 
-    public function __construct() {
-        self::$instanceCount++;
-    } // Increment the instanceCount property by one each time a new instance of the class is created
+public function __construct() {
+  self::$instanceCount++;
+} // Increment the instanceCount property by one each time a new instance of the class is created
 
-    public function canFly() {
-        return $this->flying == "yes" ? "bird can fly" : "cannot fly and is stuck on the ground"; // ternary operator
-    }
+public function canFly() {
+  return $this->flying == "yes" ? "bird can fly" : "cannot fly and is stuck on the ground"; // ternary operator
+}
 
-    public static function create() {
-        return new static();
-    }
+public static function create() {
+  return new static();
+}
 }
 
 class YellowBelliedFlyCatcher extends Bird {
 
-    public static $eggNum = "3-4, sometimes 5"; // A static property to keep track of the number of eggs laid by all birds
+public static $eggNum = "3-4, sometimes 5"; // A static property to keep track of the number of eggs laid by all birds
 
-    public $name = "yellow-bellied flycatcher";
-    public $diet = "mostly insects.";
-    public $song = "flat chilk";
+public $name = "yellow-bellied flycatcher";
+public $diet = "mostly insects.";
+public $song = "flat chilk";
 }
 class Kiwi extends Bird {
-    public $name = "kiwi";
-    public $diet = "omnivorous";
-    public $flying = "no";
+public $name = "kiwi";
+public $diet = "omnivorous";
+public $flying = "no";
 }
--- asgn03/index.php
+++ asgn03/index.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
+
+  <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>asgn03 Static</title>
-</head>
-<body>
-<h1>Static Examples</h1>
+  </head>
 
-<?php 
-    include 'Bird.php';
+  <body>
+    <h1>Static Examples</h1>
+
+    <?php 
+      include 'Bird.php';
     
-    // Display instance counts before using create() method
-    echo "<p>Bird instances: " . Bird::$instanceCount . "</p>";
-    echo "<p>Yellow-bellied Flycatcher instances: " . YellowBelliedFlyCatcher::$instanceCount . "</p>";
-    echo "<p>Kiwi instances: " . Kiwi::$instanceCount . "</p>";  
+      // Display instance counts before using create() method
+      echo "<p>Bird instances: " . Bird::$instanceCount . "</p>";
+      echo "<p>Yellow-bellied Flycatcher instances: " .      YellowBelliedFlyCatcher::$instanceCount . "</p>";
+      echo "<p>Kiwi instances: " . Kiwi::$instanceCount . "</p>";  
 
-    echo "<hr>";
+      echo "<hr>";
 
-    // Create new instances using create() method
-    $bird = Bird::create();
-    $flyCatcher = YellowBelliedFlyCatcher::create();
-    $kiwi = Kiwi::create();
+      // Create new instances using create() method
+      $bird = Bird::create();
+      $flyCatcher = YellowBelliedFlyCatcher::create();
+      $kiwi = Kiwi::create();
 
-    // Display instance counts after using create() method
-    echo "<p>Bird instances: " . Bird::$instanceCount . "</p>";
-    echo "<p>Yellow-bellied Flycatcher instances: " . YellowBelliedFlyCatcher::$instanceCount . "</p>";
-    echo "<p>Kiwi instances: " . Kiwi::$instanceCount . "</p>";
+  // Display instance counts after using create() method
+      echo "<p>Bird instances: " . Bird::$instanceCount . "</p>";
+      echo "<p>Yellow-bellied Flycatcher instances: " .      YellowBelliedFlyCatcher::$instanceCount . "</p>";
+      echo "<p>Kiwi instances: " . Kiwi::$instanceCount . "</p>";
 
-    echo "<hr>";
+      echo "<hr>";
 
-    echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
-    echo '<p>The song of the ' . $flyCatcher->name . ' on breeding ground is "' . $flyCatcher->song . '".</p>';
-    $kiwi->flying = "no";
-    echo "<p>The " . $flyCatcher->name . " " . $flyCatcher->canFly() . ".</p>";
-    echo "<p>The " . $kiwi->name. " " . $kiwi->canFly() . ".</p>";
-
-
-?>
-    </body>
+      echo '<p>The generic song of any bird is "' . $bird->song . '".</p>';
+      echo '<p>The song of the ' . $flyCatcher->name . ' on breeding ground is "' .      $flyCatcher->song . '".</p>';
+      $kiwi->flying = "no";
+      echo "<p>The " . $flyCatcher->name . " " . $flyCatcher->canFly() . ".</p>";
+      echo "<p>The " . $kiwi->name. " " . $kiwi->canFly() . ".</p>";
+    ?>
+  </body>
 </html>
 
