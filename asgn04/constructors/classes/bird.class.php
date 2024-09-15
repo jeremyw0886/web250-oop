4d828e820087d82adb0532b5678b19e9f4131458
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-15 12:10:57 -0400

    Creates bird.class.php

---
+++ asgn04/constructors/classes/bird.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Bird {
+  public $commonName;
+  public $latinName;
+
+  public function __construct($commonName, $latinName) {
+    $this->commonName = $commonName;
+    $this->latinName = $latinName;
+  }
+}
+
+?>
