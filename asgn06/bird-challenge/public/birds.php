17f2f746b2bf08589b04c51c6c24263531bfd6b7
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-24 13:52:22 -0400

    Creates a table row that lists out all of the bird properties.

--- asgn06/bird-challenge/public/birds.php
+++ asgn06/bird-challenge/public/birds.php
@@ -7,9 +7,7 @@
 <h2>Bird inventory</h2>
 <p>This is a short list -- start your birding!</p>
 
-<!-- Create a table. The header should reflect the headings in the wnc-birds.csv class. 
-     Use a table border of 1 to make the display easier to read. -->
-
+<!-- Create a table. The header should reflect the headings in the wnc-birds.csv class. Use a table border of 1 to make the display easier to read. -->
 <table border="1">
   <tr>
     <th>Common Name</th>
@@ -34,17 +32,20 @@
     $bird = new Bird($args);
 ?>
   
-  
-
 <!-- Create a table row that lists out all of the bird properties. -->
+  <tr>
+    <td><?php echo h($bird->commonName); ?></td>
+    <td><?php echo h($bird->habitat); ?></td>
+    <td><?php echo h($bird->food); ?></td>
+    <td><?php echo h($bird->nestPlacement); ?></td>
+    <td><?php echo h($bird->behavior); ?></td>
+    <td><?php echo h($bird->conservation()); ?></td>
+    <td><?php echo h($bird->backyardTips); ?></td>
+  </tr>
 
+  <?php } ?>
 
-  
-  ?>
-      
-      <?php } ?>
-
-    </table>
+</table>
 
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
