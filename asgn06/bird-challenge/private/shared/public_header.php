fab91f88488d2c9e5971e21d8b77a4aa36826e6d
jeremyw0886 <jeremyw0815@icloud.com>
2024-09-24 14:00:17 -0400

    Bird Challenge Complete

--- asgn06/bird-challenge/private/shared/public_header.php
+++ asgn06/bird-challenge/private/shared/public_header.php
@@ -2,7 +2,7 @@
 
 <html lang="en">
   <head>
-    <title>WNC Birds <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
+    <title>WNC Birds - Bird Challenge<?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
     <meta charset="utf-8">
   </head>
 
