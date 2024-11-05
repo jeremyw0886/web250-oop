709d375e252caf99790a761915b98f2bc63e8898
jeremyw0886 <jeremyw0815@icloud.com>
2024-11-05 16:22:29 -0500

    in bird.class.php - implements active record pattern, adds database interaction methods, creates bird properties and methods, adds conversation status handling, implements constructor with args

--- web250-oopdb/asgn07-active-birds/private/classes/bird.class.php
+++ web250-oopdb/asgn07-active-birds/private/classes/bird.class.php
@@ -1,7 +1,42 @@
 <?php
 
-class Bird
-{
+class Bird {
+  // ----- START ACTIVE RECORD CODE -----
+  static protected $database;
+
+  static public function set_database($database) {
+    self::$database = $database;
+  }
+
+  static public function find_by_sql($sql) {
+    $result = self::$database->query($sql);
+    if(!$result) {
+      exit("Database query failed.");
+    }
+
+    $object_array = [];
+    while($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+    $result->free();
+    return $object_array;
+  }
+
+  static public function find_all() {
+    $sql = "SELECT * FROM birds";
+    return self::find_by_sql($sql);
+  }
+
+  static protected function instantiate($record) {
+    $object = new self;
+    foreach($record as $property => $value) {
+      if(property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+    return $object;
+  }
+  // ----- END ACTIVE RECORD CODE -----
 
   public $common_name;
   public $habitat;
