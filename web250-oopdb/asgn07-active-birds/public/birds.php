022d58e8fcbd100cb1076c8b5265bfe636cfa037
jeremyw0886 <jeremyw0815@icloud.com>
2024-11-05 17:10:30 -0500

    removes wnc-birds.csv, modified birds.php and index.php for syling

--- web250-oopdb/asgn07-active-birds/public/birds.php
+++ web250-oopdb/asgn07-active-birds/public/birds.php
@@ -4,41 +4,39 @@ $page_title = 'Sightings';
 include(SHARED_PATH . '/public_header.php');
 ?>
 
-<h2>Bird inventory</h2>
-<p>This is a short list -- start your birding!</p>
+<div id="main">
+    <div id="page">
+        <div class="intro">
+            <h2>Bird Inventory</h2>
+            <p>This is a short list -- start your birding!</p>
+        </div>
 
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Habitat</th>
+                <th>Food</th>
+                <th>Conservation Status</th>
+                <th>Backyard Tips</th>
+            </tr>
 
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Habitat</th>
-    <th>Food</th>
-    <th>Nest Placement</th>
-    <th>Behavior</th>
-    <th>Conservation</th>
-    <th>Backyard Tips</th>
-  </tr>
-
-  <?php
-
-  $parser = new ParseCSV(PRIVATE_PATH . '/wnc-birds.csv');
-  $bird_array = $parser->parse();
-
-  foreach ($bird_array as $args) {
-    $bird = new Bird($args);
-  ?>
-    <tr>
-      <td><?php echo h($bird->common_name); ?></td>
-      <td><?php echo h($bird->habitat); ?></td>
-      <td><?php echo h($bird->food); ?></td>
-      <td><?php echo h($bird->nest_placement); ?></td>
-      <td><?php echo h($bird->behavior); ?></td>
-      <td><?php echo h($bird->conservation()); ?></td>
-      <td><?php echo h($bird->backyard_tips); ?></td>
-    </tr>
-  <?php } ?>
-
-</table>
+            <?php
+            $birds = Bird::find_all();
+            foreach($birds as $bird) {
+            ?>
+                <tr>
+                    <td><?php echo h($bird->common_name); ?></td>
+                    <td><?php echo h($bird->habitat); ?></td>
+                    <td><?php echo h($bird->food); ?></td>
+                    <td class="status-<?php echo strtolower(str_replace(' ', '-', $bird->conservation())); ?>">
+                        <?php echo h($bird->conservation()); ?>
+                    </td>
+                    <td><?php echo h($bird->backyard_tips); ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+</div>
 
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
