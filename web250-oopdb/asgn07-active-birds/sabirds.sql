ee4f57cc80b6f79fbaba69beff6f62d23755ca53
jeremyw0886 <jeremyw0815@icloud.com>
2024-11-05 17:12:52 -0500

    changes hyphens to normal hyphens

--- web250-oopdb/asgn07-active-birds/sabirds.sql
+++ web250-oopdb/asgn07-active-birds/sabirds.sql
@@ -35,7 +35,7 @@ INSERT INTO `birds` (`id`, `common_name`, `habitat`, `food`, `conservation_id`, 
 (1, 'Carolina Wren', 'Open woodlands', 'Insects', 1, 'Carolina Wrens visit suet-filled feeders during winter.'),
 (2, 'Tufted Titmouse', 'Forests', 'Insects', 1, 'Tufted Titmouse are regulars at backyard bird feeders, especially in winter. They prefer sunflower seeds but will eat suet, peanuts, and other seeds as well.'),
 (3, 'Ruby-Throated Hummingbird', 'Open woodlands', 'Nectar', 1, 'You can attract Ruby-throated Hummingbirds to your backyard by setting up hummingbird feeders or by planting tubular flowers.'),
-(4, 'Eastern Towhee', 'Scrub', 'Omnivore', 1, 'Eastern Towhees are likely to visit – or perhaps live in – your yard if you’ve got brushy, shrubby, or overgrown borders.'),
+(4, 'Eastern Towhee', 'Scrub', 'Omnivore', 1, 'Eastern Towhees are likely to visit - or perhaps live in - your yard if you’ve got brushy, shrubby, or overgrown borders.'),
 (5, 'Indigo Bunting', 'Open woodlands', 'Insects', 1, 'You can attract Indigo Buntings to your yard with feeders, particularly with small seeds such as thistle or nyjer.');
 
 -- --------------------------------------------------------
