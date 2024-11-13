51eb37cf397e14ad53a152764438192931034871
jeremyw0886 <jeremyw0815@icloud.com>
2024-11-13 08:38:09 -0500

    updated header to include custom css and completed delete.php

--- web250-oopdb/asgn08-active-birds/private/shared/public_header.php
+++ web250-oopdb/asgn08-active-birds/private/shared/public_header.php
@@ -4,6 +4,8 @@
   <head>
     <title>WNC Birds <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/css/styles.css'); ?>" />
   </head>
 
   <body>
