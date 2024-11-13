2d6075d4c4ed75893b1e9d170aba32106b48dbc0
jeremyw0886 <jeremyw0815@icloud.com>
2024-11-13 08:40:04 -0500

    complete edit.php file

--- web250-oopdb/asgn08-active-birds/public/active-record/edit.php
+++ web250-oopdb/asgn08-active-birds/public/active-record/edit.php
@@ -1,16 +1,47 @@
 <?php
+require_once('../../private/initialize.php');
 
-require_once('../private/initialize.php');
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/birds.php'));
+}
+$id = $_GET['id'];
+$bird = Bird::find_by_id($id);
+if($bird == false) {
+  redirect_to(url_for('/birds.php'));
+}
 
-/* 
-  Use the bicycles/staff/edit.php file as a guide 
-  so your file mimics the same functionality.
-  Be sure to include the display_errors() function.
-*/
+if(is_post_request()) {
+  // Save record using post parameters
+  $args = $_POST['bird'];
+  $bird->merge_attributes($args);
+  $result = $bird->save();
 
-<?php $page_title = 'Edit Bird'; ?>
-<?php include(SHARED_PATH . '/public_header.php'); ?>
+  if($result === true) {
+    $_SESSION['message'] = 'The bird was updated successfully.';
+    redirect_to(url_for('/active-record/show.php?id=' . $id));
+  }
+}
 
+$page_title = 'Edit Bird';
+include(SHARED_PATH . '/public_header.php');
+?>
 
+<div id="content">
+  <a class="back-link" href="<?php echo url_for('/birds.php'); ?>">&laquo; Back to List</a>
+
+  <div class="bird edit">
+    <h1>Edit Bird</h1>
+
+    <?php echo display_errors($bird->errors); ?>
+
+    <form action="<?php echo url_for('/active-record/edit.php?id=' . h(u($id))); ?>" method="post">
+      <?php include('form_fields.php'); ?>
+
+      <div id="operations">
+        <input type="submit" value="Edit Bird" />
+      </div>
+    </form>
+  </div>
+</div>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
