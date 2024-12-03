abaec35a3b1cc7a8d38ed4682b3cbe8c71a7ca5f
jeremyw0886 <jeremyw0815@icloud.com>
2024-12-02 22:53:05 -0500

    completes initialize.php

--- web250-oopdb/asgn10-active-birds/private/initialize.php
+++ web250-oopdb/asgn10-active-birds/private/initialize.php
@@ -1,32 +1,36 @@
 <?php
+ob_start();
 
-  ob_start(); 
+// Assign file paths to PHP constants
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+define("PUBLIC_PATH", PROJECT_PATH . '/public');
+define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
-  define("PRIVATE_PATH", dirname(__FILE__));
-  define("PROJECT_PATH", dirname(PRIVATE_PATH));
-  define("PUBLIC_PATH", PROJECT_PATH . '/public');
-  define("SHARED_PATH", PRIVATE_PATH . '/shared');
+// Assign the root URL to a PHP constant
+$public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+$doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+define("WWW_ROOT", $doc_root);
 
-  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
-  $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
-  define("WWW_ROOT", $doc_root);
+require_once('functions.php');
+require_once('validation_functions.php');
+require_once('db_credentials.php');
+require_once('classes/database.class.php');
+require_once('classes/databaseobject.class.php');
+require_once('classes/bird.class.php');
+require_once('classes/member.class.php');
+require_once('classes/session.class.php');
 
-  require_once('functions.php');
-  require_once('status_error_functions.php');
-  require_once('db_credentials.php');
-  require_once('database_functions.php');
-  require_once('validation_functions.php');
+// Initialize Database connection
+Database::db_connect();
+$database = Database::db_connect();
+DatabaseObject::set_database($database);
 
-  foreach(glob('classes/*.class.php') as $file) {
-    require_once($file);
-  }
+$session = new Session;
 
-  function my_autoload($class) {
-    if(preg_match('/\A\w+\Z/', $class)) {
-      include('classes/' . $class . '.class.php');
+function require_login() {
+    global $session;
+    if(!$session->is_logged_in()) {
+        redirect_to(url_for('/members/login.php'));
     }
-  }
-  spl_autoload_register('my_autoload');
-
-  $database = db_connect();
-  DatabaseObject::set_database($database);
+}
