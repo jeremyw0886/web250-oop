2d5a00b89bad76fd818d021a8a7b0f53dd70d60e
jeremyw0886 <jeremyw0815@icloud.com>
2024-12-11 17:23:56 -0500

    updates member.class and session.class

--- asgn11-three-views/private/classes/member.class.php
+++ asgn11-three-views/private/classes/member.class.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Class Member
  * @method static Member|false find_by_id(int $id)
  */
-class Member extends DatabaseObject {
+class Member extends DatabaseObject
+{
     static protected $table_name = "users";
     static protected $db_columns = ['id', 'first_name', 'last_name', 'email', 'username', 'user_level', 'hashed_password'];
 
@@ -16,8 +18,19 @@ class Member extends DatabaseObject {
     public $hashed_password;
     public $password;
     public $confirm_password;
+    public $password_required = true;
 
-    public function __construct($args=[]) {
+    /**
+     * Get the member's full name
+     * @return string
+     */
+    public function full_name()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    public function __construct($args = [])
+    {
         $this->first_name = $args['first_name'] ?? '';
         $this->last_name = $args['last_name'] ?? '';
         $this->email = $args['email'] ?? '';
@@ -27,59 +40,99 @@ class Member extends DatabaseObject {
         $this->confirm_password = $args['confirm_password'] ?? '';
     }
 
-    public function verify_password($password) {
+    public function verify_password($password)
+    {
         return password_verify($password, $this->hashed_password);
     }
 
-    /**
-     * Find a member by username
-     * @param string $username The username to search for
-     * @return Member|false The found member or false if not found
-     */
-    public static function find_by_username($username) {
+    protected function set_hashed_password()
+    {
+        $this->hashed_password = password_hash($this->password, PASSWORD_BCRYPT);
+    }
+
+    protected function create()
+    {
+        $this->set_hashed_password();
+        return parent::create();
+    }
+
+    protected function update()
+    {
+        if ($this->password != '') {
+            $this->set_hashed_password();
+        }
+        return parent::update();
+    }
+
+    protected function validate()
+    {
+        $this->errors = [];
+
+        if (is_blank($this->first_name)) {
+            $this->errors['first_name'] = "First name cannot be blank.";
+        }
+        if (is_blank($this->last_name)) {
+            $this->errors['last_name'] = "Last name cannot be blank.";
+        }
+        if (is_blank($this->email)) {
+            $this->errors['email'] = "Email cannot be blank.";
+        } elseif (!has_valid_email_format($this->email)) {
+            $this->errors['email'] = "Email must be a valid format.";
+        }
+        if (is_blank($this->username)) {
+            $this->errors['username'] = "Username cannot be blank.";
+        } elseif (!has_length($this->username, ['min' => 4, 'max' => 255])) {
+            $this->errors['username'] = "Username must be between 4 and 255 characters.";
+        }
+
+        // Only validate password if it's a new record or if password is being updated
+        if ($this->is_new() || !is_blank($this->password)) {
+            if (is_blank($this->password)) {
+                $this->errors['password'] = "Password cannot be blank.";
+            } elseif (!has_length($this->password, ['min' => 6])) {
+                $this->errors['password'] = "Password must contain 6 or more characters";
+            } elseif (!preg_match('/[A-Z]/', $this->password)) {
+                $this->errors['password'] = "Password must contain at least 1 uppercase letter";
+            } elseif (!preg_match('/[a-z]/', $this->password)) {
+                $this->errors['password'] = "Password must contain at least 1 lowercase letter";
+            } elseif (!preg_match('/[0-9]/', $this->password)) {
+                $this->errors['password'] = "Password must contain at least 1 number";
+            }
+
+            if (is_blank($this->confirm_password)) {
+                $this->errors['confirm_password'] = "Confirm password cannot be blank.";
+            } elseif ($this->password !== $this->confirm_password) {
+                $this->errors['confirm_password'] = "Password and confirm password must match.";
+            }
+        }
+
+        return $this->errors;
+    }
+
+    public function is_new()
+    {
+        return !isset($this->id);
+    }
+
+    public static function find_by_username($username)
+    {
         $sql = "SELECT * FROM " . static::$table_name . " ";
         $sql .= "WHERE username='" . self::$database->escape_string($username) . "'";
         $obj_array = static::find_by_sql($sql);
-        if(!empty($obj_array)) {
+        if (!empty($obj_array)) {
             return array_shift($obj_array);
         } else {
             return false;
         }
     }
 
-    protected function validate() {
-        $this->errors = [];
+    public function is_admin()
+    {
+        return $this->user_level === 'a';
+    }
 
-        if(is_blank($this->first_name)) {
-            $this->errors[] = "First name cannot be blank.";
-        }
-        if(is_blank($this->last_name)) {
-            $this->errors[] = "Last name cannot be blank.";
-        }
-        if(is_blank($this->email)) {
-            $this->errors[] = "Email cannot be blank.";
-        } elseif (!has_valid_email_format($this->email)) {
-            $this->errors[] = "Email must be a valid format.";
-        }
-        if(is_blank($this->username)) {
-            $this->errors[] = "Username cannot be blank.";
-        }
-        if(is_blank($this->password)) {
-            $this->errors[] = "Password cannot be blank.";
-        } elseif (strlen($this->password) < 6) {
-            $this->errors[] = "Password must contain at least 6 characters";
-        } elseif (!preg_match('/[A-Z]/', $this->password)) {
-            $this->errors[] = "Password must contain at least 1 uppercase letter";
-        } elseif (!preg_match('/[a-z]/', $this->password)) {
-            $this->errors[] = "Password must contain at least 1 lowercase letter";
-        } elseif (!preg_match('/[0-9]/', $this->password)) {
-            $this->errors[] = "Password must contain at least 1 number";
-        }
-
-        if($this->password !== $this->confirm_password) {
-            $this->errors[] = "Password and confirm password must match.";
-        }
-
-        return $this->errors;
+    public function user_level_text()
+    {
+        return $this->user_level === 'a' ? 'Admin' : 'Member';
     }
 }
--- asgn11-three-views/private/classes/session.class.php
+++ asgn11-three-views/private/classes/session.class.php
@@ -1,69 +1,103 @@
 <?php
-class Session {
+class Session
+{
     private $member_id;
     public $username;
     private $last_login;
+    private $user_level;
     public $message;
 
-    public function __construct() {
+    public function __construct()
+    {
         session_start();
         $this->check_stored_login();
         $this->check_message();
     }
 
-    public function login($member) {
-        if($member) {
-            // Prevent session fixation attacks
+    public function get_member_id()
+    {
+        return $this->member_id;
+    }
+
+    public function is_admin()
+    {
+        return isset($this->user_level) && $this->user_level === 'a';
+    }
+
+    public function login($member)
+    {
+        if ($member) {
             session_regenerate_id();
             $this->member_id = $_SESSION['member_id'] = $member->id;
             $this->username = $_SESSION['username'] = $member->username;
+            $this->user_level = $_SESSION['user_level'] = $member->user_level; // Add this line
             $this->last_login = $_SESSION['last_login'] = time();
         }
         return true;
     }
 
-    public function is_logged_in() {
+    public function is_logged_in()
+    {
         return isset($this->member_id) && $this->last_login_is_recent();
     }
 
-    public function logout() {
-        unset($_SESSION['member_id']);
-        unset($_SESSION['username']);
-        unset($_SESSION['last_login']);
-        unset($this->member_id);
-        unset($this->username);
-        unset($this->last_login);
-        return true;
+    public function logout()
+    {
+        // Unset all session variables
+        $_SESSION = array();
+
+        // Destroy the session cookie
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time() - 42000, '/');
+        }
+
+        // Destroy the session
+        session_destroy();
+
+        // Reset object properties
+        $this->member_id = null;
+        $this->username = null;
+        $this->last_login = null;
+        $this->user_level = null;
     }
 
-    private function check_stored_login() {
-        if(isset($_SESSION['member_id'])) {
+    private function check_stored_login()
+    {
+        if (isset($_SESSION['member_id'])) {
             $this->member_id = $_SESSION['member_id'];
             $this->username = $_SESSION['username'];
+            $this->user_level = $_SESSION['user_level'];
             $this->last_login = $_SESSION['last_login'];
         }
     }
 
-    private function last_login_is_recent() {
-        if(!isset($this->last_login)) {
+    private function last_login_is_recent()
+    {
+        if (!isset($this->last_login)) {
             return false;
         }
         return ($this->last_login + (60 * 60 * 24)) >= time();
     }
 
-    public function message($msg="") {
-        if(!empty($msg)) {
-            // Set message
+    public function message($msg = "")
+    {
+        if (!empty($msg)) {
             $_SESSION['message'] = $msg;
             return true;
         } else {
-            // Get message
             return $this->message;
         }
     }
 
-    private function check_message() {
-        if(isset($_SESSION['message'])) {
+    public function clear_message()
+    {
+        $this->message = '';
+        unset($_SESSION['message']);
+    }
+
+    private function check_message()
+    {
+        if (isset($_SESSION['message'])) {
             $this->message = $_SESSION['message'];
             unset($_SESSION['message']);
         } else {
