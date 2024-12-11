11fe66a217bd69ed9883deed34ad2f16b4e4c0ab
jeremyw0886 <jeremyw0815@icloud.com>
2024-12-11 17:25:25 -0500

    updates public_header

--- asgn11-three-views/private/shared/public_header.php
+++ asgn11-three-views/private/shared/public_header.php
@@ -1,33 +1,28 @@
-<?php
-  if(!isset($page_title)) { $page_title = 'WNC Birds'; }
-?>
-
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>WNC Birds - <?php echo h($page_title); ?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/style.css'); ?>">
-  </head>
 
-  <body>
-    <header>
-      <h1><a href="<?php echo url_for('/index.php'); ?>">WNC Birds</a></h1>
-      <nav>
-        <ul>
-          <li><a href="<?php echo url_for('/index.php'); ?>">Home</a></li>
-          <li><a href="<?php echo url_for('/birds/index.php'); ?>">Birds</a></li>
-          <li><a href="<?php echo url_for('/about.php'); ?>">About</a></li>
-          <?php if($session->is_logged_in()) { ?>
-            <li><a href="<?php echo url_for('/birds/new.php'); ?>">Add Bird</a></li>
-            <li>Welcome, <?php echo h($session->username); ?></li>
-            <li><a href="<?php echo url_for('/members/logout.php'); ?>">Logout</a></li>
-          <?php } else { ?>
-            <li><a href="<?php echo url_for('/members/login.php'); ?>">Login</a></li>
-          <?php } ?>
-        </ul>
-      </nav>
-    </header>
+<head>
+  <title>WNC Birds - <?php echo h($page_title); ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/style.css'); ?>">
+</head>
 
-    <?php echo display_session_message(); ?>
+<header>
+  <h1><a href="<?php echo url_for('/index.php'); ?>">WNC Birds</a></h1>
+  <nav>
+    <ul>
+      <li><a href="<?php echo url_for('/index.php'); ?>">Home</a></li>
+      <li><a href="<?php echo url_for('/birds/index.php'); ?>">View Birds</a></li>
+      <?php if ($session->is_logged_in()) { ?>
+        <?php if ($session->is_admin()) { ?>
+          <li><a href="<?php echo url_for('/members/index.php'); ?>">Manage Birds/Members</a></li>
+        <?php } ?>
+        <li><a href="<?php echo url_for('/logout.php'); ?>">Logout, <?php echo h($session->username); ?> (<?php echo $session->is_admin() ? 'Admin' : 'Member'; ?>)</a></li>
+      <?php } else { ?>
+        <li><a href="<?php echo url_for('/login.php'); ?>">Login</a></li>
+        <li><a href="<?php echo url_for('/signup.php'); ?>">Sign Up</a></li>
+      <?php } ?>
+    </ul>
+  </nav>
+</header>
